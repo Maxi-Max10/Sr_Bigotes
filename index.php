9bb2751d42ae24ff982f4176030f33ede941184a
Maxi-Max10 <maxgiu3@gmail.com>
2022-10-17 23:29:18 -0300

    inde

--- index.php
+++ index.php
@@ -229,40 +229,7 @@
         </div>
     </section>
 
-    <!-- TEAM SECTION 
-
-    <section id="team" class="team_section">
-        <div class="container">
-            <div class="section_heading ">
-                
-                <h2>Nuestros Peluqueros</h2>
-                <div class=""></div>
-            </div>
-            <ul class="team_members row"> 
-                <li class="col-lg-3 col-md-6 padd_col_res">
-                    <div class="team_member">
-                        <img src="Design/images/team-1.jpg" alt="Team Member">
-                    </div>
-                </li>
-                <li class="col-lg-3 col-md-6 padd_col_res">
-                    <div class="team_member">
-                        <img src="Design/images/team-2.jpg" alt="Team Member">
-                    </div>
-                </li>
-                <li class="col-lg-3 col-md-6 padd_col_res">
-                    <div class="team_member">
-                        <img src="Design/images/team-3.jpg" alt="Team Member">
-                    </div>
-                </li>
-                 <li class="col-lg-3 col-md-6 padd_col_res">
-                    <div class="team_member">
-                        <img src="Design/images/team-4.jpg" alt="Team Member">
-                    </div>
-                </li> 
-            </ul>
-        </div>
-    </section>
-    -->
+   
 
     <!-- REVIEWS SECTION -->
 
