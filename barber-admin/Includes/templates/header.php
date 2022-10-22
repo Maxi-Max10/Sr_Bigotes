732bd362d9b2844c3bf8867d7cf3df0880113495
= <=>
2022-10-22 18:48:39 -0300

    001

--- barber-admin/Includes/templates/header.php
+++ barber-admin/Includes/templates/header.php
@@ -40,7 +40,7 @@
 					<div class="sidebar-brand-icon rotate-n-15">
 			  			<i class="bs bs-scissors-1"></i>
 					</div>
-					<div class="sidebar-brand-text mx-3">Sr.Bigotes</div>g
+					<div class="sidebar-brand-text mx-3">Sr.Bigotes</div>
 		  		</a>
 
 		  		<!-- Divider -->
